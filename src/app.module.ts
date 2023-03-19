607146b96c06574a76e9f39b53f65fb2f5e85fd7
Maximo Solis Farfan <maxsolfar@gmail.com>
2023-03-19 00:14:51 -0500

    Feat: ENV, CORS configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+      isGlobal: true,
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}

a4e63a52541eee706e2ad6bb215992e4119fdf29
Maximo Solis Farfan <maxsolfar@gmail.com>
2023-03-20 01:33:01 -0500

    Feat: TypeORM configuration, it connects with Postgres in Docker Container | closes #1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSourceConfig } from './config/data.source';
 
 @Module({
   imports: [
@@ -8,6 +10,7 @@ import { ConfigModule } from '@nestjs/config';
       envFilePath: `.${process.env.NODE_ENV}.env`,
       isGlobal: true,
     }),
+    TypeOrmModule.forRoot({ ...DataSourceConfig }),
     UsersModule,
   ],
 })
---
+++ src/config/data.source.ts
@@ -0,0 +1,33 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { DataSourceOptions, DataSource } from 'typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+
+ConfigModule.forRoot({
+  envFilePath: `.${process.env.NODE_ENV}.env`,
+});
+
+const configService = new ConfigService();
+
+console.log(
+  configService.get('DB_HOST'),
+  configService.get('DB_USER'),
+  configService.get('DB_NAME'),
+  configService.get('DB_PORT'),
+  configService.get('DB_PASSWORD'),
+);
+export const DataSourceConfig: DataSourceOptions = {
+  type: 'postgres',
+  host: configService.get('DB_HOST'),
+  port: configService.get('DB_PORT'),
+  username: configService.get('DB_USER'),
+  password: configService.get('DB_PASSWORD'),
+  database: configService.get('DB_NAME'),
+  entities: [__dirname + '/../**/**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/../../migrations/*{.ts,.js}'],
+  synchronize: false,
+  migrationsRun: true,
+  logging: false,
+  namingStrategy: new SnakeNamingStrategy(),
+};
+
+export const AppDS = new DataSource(DataSourceConfig);
